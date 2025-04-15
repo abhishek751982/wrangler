ed795a5e92a1fa7168e48ffeac9214149d67eeb3
Abhishek Sharma <abhishek751982@gmail.com>
2025-04-16 01:57:16 +0530

    Add unit tests for ByteSize and TimeDuration parsing; enhance GrammarBasedParserTest and RecipeCompilerTest for AggregateStats validation

--- wrangler-core/src/test/java/io/cdap/wrangler/parser/RecipeCompilerTest.java
+++ wrangler-core/src/test/java/io/cdap/wrangler/parser/RecipeCompilerTest.java
@@ -37,10 +37,10 @@ public class RecipeCompilerTest {
     try {
       Compiler compiler = new RecipeCompiler();
       CompileStatus status = compiler.compile(
-          "parse-as-csv :body ' ' true;\n"
-        + "set-column :abc, :edf;\n"
-        + "send-to-error exp:{ window < 10 } ;\n"
-        + "parse-as-simple-date :col 'yyyy-mm-dd' :col 'test' :col2,:col4,:col9 10 exp:{test < 10};\n"
+            "parse-as-csv :body ' ' true;\n"
+          + "set-column :abc, :edf;\n"
+          + "send-to-error exp:{ window < 10 } ;\n"
+          + "parse-as-simple-date :col 'yyyy-mm-dd' :col 'test' :col2,:col4,:col9 10 exp:{test < 10};\n"
       );
 
       Assert.assertNotNull(status.getSymbols());
@@ -53,10 +53,10 @@ public class RecipeCompilerTest {
   @Test
   public void testMacroSkippingDuringParsing() throws Exception {
     String[] recipe = new String[] {
-      "parse-as-csv :body ',' true;",
-      "${macro1}",
-      "${macro${number}}",
-      "parse-as-csv :body '${delimiter}' true;"
+       "parse-as-csv :body ',' true;",
+       "${macro1}",
+       "${macro${number}}",
+       "parse-as-csv :body '${delimiter}' true;"
     };
 
     CompileStatus status = TestingRig.compile(recipe);
@@ -66,7 +66,7 @@ public class RecipeCompilerTest {
   @Test
   public void testSingleMacroLikeWranglerPlugin() throws Exception {
     String[] recipe = new String[] {
-      "${directives}"
+       "${directives}"
     };
 
     CompileStatus status = TestingRig.compile(recipe);
@@ -76,9 +76,9 @@ public class RecipeCompilerTest {
   @Test
   public void testSparedPragmaLoadDirectives() throws Exception {
     String[] recipe = new String[] {
-      "#pragma load-directives test1,test2,test3,test4,test5;",
-      "${directives}",
-      "#pragma load-directives root1,root2,root3;"
+       "#pragma load-directives test1,test2,test3,test4,test5;",
+       "${directives}",
+       "#pragma load-directives root1,root2,root3;"
     };
     TestingRig.compileSuccess(recipe);
   }
@@ -86,8 +86,8 @@ public class RecipeCompilerTest {
   @Test
   public void testNestedMacros() throws Exception {
     String[] recipe = new String[] {
-      "#pragma load-directives test1,test2,test3,test4,test5;",
-      "${directives_${number}}"
+       "#pragma load-directives test1,test2,test3,test4,test5;",
+       "${directives_${number}}"
     };
     TestingRig.compileSuccess(recipe);
   }
@@ -95,8 +95,8 @@ public class RecipeCompilerTest {
   @Test
   public void testSemiColonMissing() throws Exception {
     String[] recipe = new String[] {
-      "#pragma load-directives test1,test2,test3,test4,test5",
-      "${directives_${number}}"
+       "#pragma load-directives test1,test2,test3,test4,test5",
+       "${directives_${number}}"
     };
     TestingRig.compileFailure(recipe);
   }
@@ -104,8 +104,8 @@ public class RecipeCompilerTest {
   @Test
   public void testMissingOpenBraceOnMacro() throws Exception {
     String[] recipe = new String[] {
-      "#pragma load-directives test1,test2,test3,test4,test5;",
-      "$directives}"
+       "#pragma load-directives test1,test2,test3,test4,test5;",
+       "$directives}"
     };
     TestingRig.compileFailure(recipe);
   }
@@ -113,8 +113,8 @@ public class RecipeCompilerTest {
   @Test
   public void testMissingCloseBraceOnMacro() throws Exception {
     String[] recipe = new String[] {
-      "#pragma load-directives test1,test2,test3,test4,test5;",
-      "${directives"
+       "#pragma load-directives test1,test2,test3,test4,test5;",
+       "${directives"
     };
     TestingRig.compileFailure(recipe);
   }
@@ -122,8 +122,8 @@ public class RecipeCompilerTest {
   @Test
   public void testMissingBothBraceOnMacro() throws Exception {
     String[] recipe = new String[] {
-      "#pragma load-directives test1,test2,test3,test4,test5;",
-      "${directives"
+       "#pragma load-directives test1,test2,test3,test4,test5;",
+       "${directives"
     };
     TestingRig.compileFailure(recipe);
   }
@@ -131,7 +131,7 @@ public class RecipeCompilerTest {
   @Test
   public void testMissingPragmaHash() throws Exception {
     String[] recipe = new String[] {
-      "pragma load-directives test1,test2,test3,test4,test5;",
+       "pragma load-directives test1,test2,test3,test4,test5;",
     };
     TestingRig.compileFailure(recipe);
   }
@@ -147,10 +147,10 @@ public class RecipeCompilerTest {
   @Test
   public void testWithIfStatement() throws Exception {
     String[] recipe = new String[] {
-      "#pragma load-directives test1,test2;",
-      "${macro_1}",
-      "if ((test > 10) && (window < 20)) {  parse-as-csv :body ',' true; if (window > 10) " +
-        "{ send-to-error exp:{test > 10}; } }"
+       "#pragma load-directives test1,test2;",
+       "${macro_1}",
+       "if ((test > 10) && (window < 20)) {  parse-as-csv :body ',' true; if (window > 10) " +
+         "{ send-to-error exp:{test > 10}; } }"
     };
     TestingRig.compileSuccess(recipe);
   }
@@ -158,14 +158,14 @@ public class RecipeCompilerTest {
   @Test
   public void testComplexExpression() throws Exception {
     String[] recipe = new String[] {
-      "parse-as-csv body , true",
-      "drop body",
-      "merge body_1 body_2 Full_Name ' '",
-      "drop body_1,body_2",
-      "find-and-replace body_4 s/Washington//g",
-      "send-to-error empty(body_4)",
-      "send-to-error body_5 =~ \"DC.*\"",
-      "filter-rows-on regex-match body_5 *as*"
+       "parse-as-csv body , true",
+       "drop body",
+       "merge body_1 body_2 Full_Name ' '",
+       "drop body_1,body_2",
+       "find-and-replace body_4 s/Washington//g",
+       "send-to-error empty(body_4)",
+       "send-to-error body_5 =~ \"DC.*\"",
+       "filter-rows-on regex-match body_5 *as*"
     };
     CompileStatus compile = TestingRig.compile(recipe);
     Assert.assertTrue(true);
@@ -174,14 +174,14 @@ public class RecipeCompilerTest {
   @Test
   public void test() throws Exception {
     String[] recipe = new String[] {
-      "parse-as-csv body , true",
-      "drop body",
-      "merge body_1 body_2 Full_Name ' '",
-      "drop body_1,body_2",
-      "find-and-replace body_4 s/Washington//g",
-      "send-to-error empty(body_4)",
-      "send-to-error body_5 =~ \"DC.*\"",
-      "filter-rows-on regex-match body_5 *as*"
+       "parse-as-csv body , true",
+       "drop body",
+       "merge body_1 body_2 Full_Name ' '",
+       "drop body_1,body_2",
+       "find-and-replace body_4 s/Washington//g",
+       "send-to-error empty(body_4)",
+       "send-to-error body_5 =~ \"DC.*\"",
+       "filter-rows-on regex-match body_5 *as*"
     };
     CompileStatus compile = TestingRig.compile(recipe);
     Assert.assertTrue(true);
@@ -190,7 +190,7 @@ public class RecipeCompilerTest {
   @Test
   public void testSingleLineDirectives() throws Exception {
     String[] recipe = new String[] {
-      "parse-as-csv :body '\t' true; drop :body;"
+       "parse-as-csv :body '\t' true; drop :body;"
     };
     CompileStatus compile = TestingRig.compile(recipe);
     Assert.assertTrue(true);
@@ -199,7 +199,7 @@ public class RecipeCompilerTest {
   @Test
   public void testError() throws Exception {
     String[] recipe = new String[] {
-      "parse-as-abababa-csv :body '\t' true; drop :body;"
+       "parse-as-abababa-csv :body '\t' true; drop :body;"
     };
     CompileStatus compile = TestingRig.compile(recipe);
     Assert.assertTrue(true);
@@ -208,11 +208,47 @@ public class RecipeCompilerTest {
   @Test
   public void testRecipePragmaWithCompiler() throws Exception {
     String[] recipe = new String[] {
-      "#pragma load-directives test1,test2,test3,test4;",
-      "${directives}"
+       "#pragma load-directives test1,test2,test3,test4;",
+       "${directives}"
     };
     CompileStatus compile = TestingRig.compile(recipe);
     Set<String> loadableDirectives = compile.getSymbols().getLoadableDirectives();
     Assert.assertEquals(4, loadableDirectives.size());
+  }
+  @Test
+  public void testValidAggregateStatsSyntax() throws Exception {
+    String[] recipe = new String[] {
+        "aggregate-stats :data_transfer_size :response_time total_size_mb total_time_sec"
+    };
+    CompileStatus status = TestingRig.compile(recipe);
+    Assert.assertTrue(status.isSuccess());
+    Assert.assertEquals(1, status.getSymbols().size()); // Assuming one directive is parsed
+  }
+
+  @Test
+  public void testInvalidAggregateStatsMissingColumns() throws Exception {
+    String[] recipe = new String[] {
+        "aggregate-stats :data_transfer_size total_size_mb total_time_sec" // Missing timeCol
+    };
+    CompileStatus status = TestingRig.compile(recipe);
+    Assert.assertFalse(status.isSuccess()); // Infer errors from lack of success
+  }
+
+  @Test
+  public void testInvalidAggregateStatsWrongType() throws Exception {
+    String[] recipe = new String[] {
+        "aggregate-stats data_transfer_size :response_time total_size_mb total_time_sec" // No : prefix
+    };
+    CompileStatus status = TestingRig.compile(recipe);
+    Assert.assertFalse(status.isSuccess()); // Infer errors from lack of success
+  }
+
+  @Test
+  public void testInvalidAggregateStatsInvalidUnit() throws Exception {
+    String[] recipe = new String[] {
+        "aggregate-stats :data_transfer_size :response_time total_size_mb total_time_sec invalid_unit"
+    };
+    CompileStatus status = TestingRig.compile(recipe);
+    Assert.assertFalse(status.isSuccess()); // Infer errors from lack of success
   }
 }
